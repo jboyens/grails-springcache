dd44a606c610f3b290315a3a6f604e430a850d19
Rob Fletcher <robert.w.fletcher@gmail.com>
2009-11-29 11:08:22 +0000

    integration tests for cache flushing

--- test/projects/springcache-test/grails-app/services/pirates/PiracyService.groovy
+++ test/projects/springcache-test/grails-app/services/pirates/PiracyService.groovy
@@ -1,6 +1,7 @@
 package pirates
 
 import grails.plugins.springcache.annotations.Cacheable
+import grails.plugins.springcache.annotations.CacheFlush
 
 class PiracyService {
 
@@ -25,5 +26,27 @@ class PiracyService {
 			ilike "name", "%$name%"
 			order "name", "asc"
 		}
+	}
+
+	@Cacheable(cacheName = "ShipCache")
+	List listShipNames() {
+		Ship.withCriteria {
+			projections {
+				property "name"
+			}
+			order "name", "asc"
+		}
+	}
+
+	@CacheFlush(cacheNames = "PirateCache")
+	void newPirate(String name) {
+		new Pirate(name: name).save(failOnError: true)
+	}
+
+	@CacheFlush(cacheNames = ["PirateCache"])
+	void newShip(String name, List crewNames) {
+		new Ship(name: name, crew: crewNames.collect {
+			Pirate.findByName(it) ?: new Pirate(name: it)
+		}).save(failOnError: true)
 	}
 }
--- test/projects/springcache-test/test/integration/pirates/CachingSpecification.groovy
+++ test/projects/springcache-test/test/integration/pirates/CachingSpecification.groovy
@@ -3,6 +3,7 @@ package pirates
 import grails.plugin.spock.IntegrationSpecification
 import net.sf.ehcache.Cache
 import pirates.Pirate
+import grails.validation.ValidationException
 
 class CachingSpecification extends IntegrationSpecification {
 
@@ -13,6 +14,7 @@ class CachingSpecification extends IntegrationSpecification {
 		Pirate.build(name: "Blackbeard")
 		Pirate.build(name: "Calico Jack")
 		Pirate.build(name: "Black Bart")
+		Ship.build(name: "Queen Anne's Revenge", crew: Pirate.findAllByName("Blackbeard"))
 	}
 
 	void cleanup() {
@@ -20,6 +22,7 @@ class CachingSpecification extends IntegrationSpecification {
 	}
 
 	void cleanupSpec() {
+		Ship.list()*.delete()
 		Pirate.list()*.delete()
 	}
 
@@ -61,6 +64,71 @@ class CachingSpecification extends IntegrationSpecification {
 		and: "The results are correct"
 		result1 == ["Calico Jack"]
 		result2 == ["Black Bart", "Blackbeard"]
+	}
+
+	void "The cache can be flushed"() {
+		given: "A cache exists"
+		def cache = new Cache("PirateCache", 100, false, true, 0, 0)
+		cacheManager.addCache(cache)
+
+		when: "A cacheable method is called"
+		def result1 = piracyService.listPirateNames()
+
+		and: "A flushing method is called"
+		piracyService.newPirate("Anne Bonny")
+
+		and: "The cacheable method is called again"
+		def result2 = piracyService.listPirateNames()
+
+		then: "The cache is not hit"
+		cache.statistics.cacheHits == 0
+
+		and: "The results from before and after flushing are different"
+		result1 == ["Black Bart", "Blackbeard", "Calico Jack"]
+		result2 == ["Anne Bonny", "Black Bart", "Blackbeard", "Calico Jack"]
+	}
+
+	void "The cache is flushed even if the flushing method fails"() {
+		given: "A cache exists"
+		def cache = new Cache("PirateCache", 100, false, true, 0, 0)
+		cacheManager.addCache(cache)
+
+		and: "The cache is primed"
+		piracyService.listPirateNames()
+		def initialCacheSize = cache.statistics.objectCount
+
+		when: "A flushing method is called with parameters that will cause it to fail"
+		piracyService.newPirate("Blackbeard")
+
+		then: "An exception is thrown by the flushing method"
+		thrown ValidationException
+
+		and: "The cache is still flushed"
+		initialCacheSize == 1
+		cache.statistics.objectCount == 0
+	}
+
+	void "Multiple caches can be flushed by a single method"() {
+		given: "Multiple caches exist"
+		def cache1 = new Cache("PirateCache", 100, false, true, 0, 0)
+		def cache2 = new Cache("ShipCache", 100, false, true, 0, 0)
+		cacheManager.addCache(cache1)
+		cacheManager.addCache(cache2)
+
+		and: "Both caches are primed"
+		piracyService.listPirateNames()
+		piracyService.listShipNames()
+		def initialCache1Size = cache1.statistics.objectCount
+		def initialCache2Size = cache2.statistics.objectCount
+
+		when: "A method is called that should flush both caches"
+		piracyService.newShip("Royal Fortune", ["Black Bart", "Walter Kennedy"])
+
+		then: "Both caches are flushed"
+		initialCache1Size == 1
+		cache1.statistics.objectCount == 0
+		initialCache2Size == 1
+		cache2.statistics.objectCount == 0
 	}
 
 }
