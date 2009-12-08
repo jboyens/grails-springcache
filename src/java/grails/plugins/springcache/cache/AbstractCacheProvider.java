2b9fcade54f37c248041088f86e902cd9dd7c2e8
Robert Fletcher <rob@serenity.(none)>
2009-12-08 17:54:23 +0000

    some javadocs

--- src/java/grails/plugins/springcache/cache/AbstractCacheProvider.java
+++ src/java/grails/plugins/springcache/cache/AbstractCacheProvider.java
@@ -20,43 +20,65 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+/**
+ * An abstract base class for cache provider implementations. Implementations just need to define how caching and
+ * flushing models are created based on configuration properties and how caches are retrieved based on models.
+ *
+ * @param <C> The specific CachingModel implementation used by the provider.
+ * @param <F> The specific FlushingModel implementation used by the provider.
+ */
 public abstract class AbstractCacheProvider<C extends CachingModel, F extends FlushingModel> implements CacheProvider {
 
 	protected final Map<String, C> cachingModels = new HashMap<String, C>();
 	protected final Map<String, F> flushingModels = new HashMap<String, F>();
 
-	protected String getRequiredProperty(Properties properties, String propertyName) {
-		String cacheName = properties.getProperty(propertyName);
-		if (cacheName == null) throw new CacheConfigurationException(String.format("Required property %s not found in %s", propertyName, properties));
-		return cacheName;
-	}
-
+	/**
+	 * Implementations should create a new CachingModel, applying the specified properties and then call {@link
+	 * #addCachingModel(CachingModel)}.
+	 */
 	public abstract void addCachingModel(String id, Properties properties);
 
+	/**
+	 * Implementations should create a new FlushingModel, applying the specified properties and then call {@link
+	 * #addFlushingModel(FlushingModel)}.
+	 */
 	public abstract void addFlushingModel(String id, Properties properties);
 
-	public void addCachingModel(C cachingModel) {
+	/**
+	 * Implementations should return the cache associated with the specified CachingModel.
+	 */
+	protected abstract CacheFacade getCache(C cachingModel);
+
+	/**
+	 * Implementations should return the caches associated with the specified FlushingModel.
+	 */
+	protected abstract Collection<CacheFacade> getCaches(F flushingModel);
+
+	public final void addCachingModel(C cachingModel) {
 		cachingModels.put(cachingModel.getId(), cachingModel);
 	}
 
-	public void addFlushingModel(F flushingModel) {
+	public final void addFlushingModel(F flushingModel) {
 		flushingModels.put(flushingModel.getId(), flushingModel);
 	}
 
-	public CacheFacade getCache(String cachingModelId) throws CacheNotFoundException {
+	public final CacheFacade getCache(String cachingModelId) throws CacheNotFoundException {
 		C cachingModel = cachingModels.get(cachingModelId);
 		if (cachingModel == null) throw new InvalidCachingModelException(cachingModelId);
 		return getCache(cachingModel);
 	}
 
-	public Collection<CacheFacade> getCaches(String flushingModelId) throws CacheNotFoundException {
+	public final Collection<CacheFacade> getCaches(String flushingModelId) throws CacheNotFoundException {
 		F flushingModel = flushingModels.get(flushingModelId);
 		if (flushingModel == null) throw new InvalidFlushingModelException(flushingModelId);
 		return getCaches(flushingModel);
 	}
 
-	protected abstract CacheFacade getCache(C cachingModel);
-
-	protected abstract Collection<CacheFacade> getCaches(F flushingModel);
+	protected final String getRequiredProperty(Properties properties, String propertyName) {
+		String cacheName = properties.getProperty(propertyName);
+		if (cacheName == null)
+			throw new CacheConfigurationException(String.format("Required property %s not found in %s", propertyName, properties));
+		return cacheName;
+	}
 
 }
